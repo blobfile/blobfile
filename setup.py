46cc32974b1bb108b528033401dd58d512669e43
Christopher Hesse <48501609+cshesse@users.noreply.github.com>
2019-06-23 23:39:13 -0700

    pypi

--- setup.py
+++ setup.py
@@ -1,11 +1,21 @@
 import os
 from setuptools import setup, find_packages
 
+SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
+
+README = open(os.path.join(SCRIPT_DIR, "README.md")).read()
+
 setup_dict = dict(
     name="blobfile",
     version="0.1",
+    description="Read GCS and local paths with the same interface, clone of tensorflow.io.gfile",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/cshesse/blobfile",
+    author="Christopher Hesse",
+    license="Public Domain",
     packages=find_packages(),
-    install_requires=["google-cloud-storage~=1.16.1"],
+    install_requires=["google-cloud-storage~=1.16"],
     extras_require={"dev": ["pytest", "tensorflow", "imageio", "imageio-ffmpeg"]},
 )
 

aafc54a054846f41e8ebb01427f8e11e4e0426ae
Christopher Hesse <48501609+cshesse@users.noreply.github.com>
2019-11-23 15:23:37 -0800

    move typeshed

--- blobfile/blobfile.py
+++ blobfile/blobfile.py
@@ -1265,9 +1265,10 @@ def BlobFile(path, mode="r", buffer_size=io.DEFAULT_BUFFER_SIZE):
     if "r" in mode:
         f = io.BufferedReader(f, buffer_size=buffer_size)
         if "b" not in mode:
+            # PR for type fix https://github.com/python/typeshed/pull/3485
             f = io.TextIOWrapper(
                 f, encoding="utf8"
-            )  # type: ignore  https://github.com/python/typeshed/pull/3485
+            )
     return f
 
 

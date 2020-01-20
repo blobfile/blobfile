af635b3784797514494088e846a56032f8d6e1e7
Christopher Hesse <48501609+cshesse@users.noreply.github.com>
2020-01-19 23:31:42 -0800

    .

--- run-tests.py
+++ run-tests.py
@@ -1,7 +1,6 @@
 import subprocess as sp
 import sys
 
-run = lambda cmd: sp.run(cmd, shell=True, check=True)
 sp.run(["pip", "install", "-e", "."], check=True)
 sp.run(
     ["pytest", "blobfile", "--typeguard-packages=blobfile"] + sys.argv[1:], check=True

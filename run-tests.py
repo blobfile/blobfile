38bd2c82870c2c028407cb70ed3bbe5419edb9d1
Christopher Hesse <48501609+cshesse@users.noreply.github.com>
2020-01-30 21:22:30 -0800

    tests

--- run-tests.py
+++ run-tests.py
@@ -7,9 +7,6 @@ sp.run(
         "pytest",
         "blobfile",
         "--typeguard-packages=blobfile",
-        "-s",
-        "-k",
-        "test_glob[_get_temp_gcs_path]",
     ]
     + sys.argv[1:],
     check=True,

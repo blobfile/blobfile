ef62af715c42bf4c7ffc3028c8b63ccfa9dbed80
Christopher Hesse <48501609+cshesse@users.noreply.github.com>
2021-01-07 22:41:03 -0800

    workaround

--- testing/run-static.py
+++ testing/run-static.py
@@ -2,6 +2,7 @@ import subprocess as sp
 import shlex
 import os
 import shutil
+import argparse
 
 
 def shell(cmd):
@@ -20,15 +21,26 @@ def format_files(dirpath, extensions, command):
 
 
 def main():
-    print("formatting python files")
-    shell(
-        [
-            "black",
-            "--target-version=py37",
-            r"--exclude=(\.eggs|\.git|\.hg|\.mypy_cache|\.nox|\.tox|\.venv|_build|buck-out|build|dist|third-party|typings)",
-            ".",
-        ]
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--check-only", action="store_true", help="only check files, don't format them"
     )
+    args = parser.parse_args()
+
+    assert (
+        open("env.yaml").read() == open("testing/env.yaml").read()
+    ), "env.yaml files are out of sync"
+
+    print("formatting python files")
+    black_cmd = ["black"]
+    if args.check_only:
+        black_cmd += ["--check"]
+    black_cmd += [
+        "--target-version=py37",
+        r"--exclude=(\.eggs|\.git|\.hg|\.mypy_cache|\.nox|\.tox|\.venv|_build|buck-out|build|dist|third-party|typings)",
+        ".",
+    ]
+    shell(black_cmd)
 
     print("checking python files")
     shell([shutil.which("pyright"), "--project", "."])

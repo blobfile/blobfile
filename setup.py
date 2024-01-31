9a47fc81aeba850624084e87689b3307f95aa918
Shantanu <12621235+hauntsaninja@users.noreply.github.com>
2024-01-31 14:22:03 -0800

    Loosen install bounds (#234)

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup_dict: Dict[str, Any] = dict(
     author="Christopher Hesse",
     license="Public Domain",
     packages=find_packages(),
-    install_requires=["pycryptodomex~=3.8", "urllib3>=1.25.3,<3", "lxml~=4.9", "filelock~=3.0"],
+    install_requires=["pycryptodomex>=3.8", "urllib3>=1.25.3,<3", "lxml>=4.9", "filelock>=3.0"],
     python_requires=">=3.8.0",
     # indicate that we have type information
     package_data={"blobfile": ["py.typed", "VERSION"]},
